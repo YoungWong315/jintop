d801db53222ca6d0854c39bb461443a7d8d058b8
1099216788@qq.com <1099216788@qq.com>
2019-04-02 16:21:21 +0800

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   // 打包目标路径 -----------------------------------------------------<
   buildDir: process.env.NODE_ENV === "production" ? "/www/wwwroot/website/www_jintop_cn/nuxt" : ".nuxt",
-  rootDir: process.env.NODE_ENV === "production" ? "/www/wwwroot/website/www_jintop_cn" : process.cwd(),
+  rootDir: process.env.NODE_ENV === "production" ? "/www/wwwroot/website/www_jintop_cn/nuxt/dist" : process.cwd(),
   // ------------------------------------------------------------<
   srcDir: "client/",
   build: {
