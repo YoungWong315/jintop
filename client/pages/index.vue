86650c5dfb876ba75cc658f7b3ea52b489a6f2ec
1099216788@qq.com <1099216788@qq.com>
2019-09-11 16:28:32 +0800

    proxy test

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -21,6 +21,56 @@
 <script>
 const _ = require('lodash')
 
+const defineReactive = obj => {
+  // obj属性如果仍是对象，需要递归将其代理才可生效
+  return new Proxy(obj, {
+    set(target, propKey, value, receiver) {
+      console.log(`setter newVal: ${value}, prop: ${propKey}`)
+      return Reflect.set(target, propKey, value, receiver)
+    },
+    get(target, propKey, receiver) {
+      console.log(`get`)
+      console.log(target, propKey)
+      if (target instanceof Date && typeof target[propKey] === 'function') {
+        // 针对Date类，方法的指向问题
+        return target[propKey].bind(target)
+      }
+      return Reflect.get(target, propKey, receiver) // 返回被代理对象的属性或方法
+    },
+    // 针对function
+    apply(target, ctx, args) {
+      return Reflect.apply(...arguments)
+    }
+  })
+}
+
+const test = {
+  a: 1,
+  b: 2,
+  fn: function(msg) {
+    return msg + this.c
+  },
+  arr: [1, 2, 3]
+}
+/* const proxy = defineReactive(test)
+proxy.c = 3
+console.log(proxy.c)
+console.log(proxy.fn('msg'))
+
+const date = new Date()
+const proxyDate = defineReactive(date)
+console.log(proxyDate)
+console.log(proxyDate.getTime()) */
+
+const proxyArr = defineReactive(test.arr)
+// 使用数组方法时会触发两次set，一次方法(push...)，一次修改length
+proxyArr.push(7)
+proxyArr[8] = 8
+proxyArr.shift()
+proxyArr.forEach(elem => {
+  console.log(elem)
+})
+
 export default {
   data() {
     return {
