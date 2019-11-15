9431751b41ecdc90510a45cb1778d84d26fd57c6
1099216788@qq.com <1099216788@qq.com>
2019-11-15 15:57:54 +0800

    找到jsdom的用法

--- server/modules/crawler/index.js
+++ server/modules/crawler/index.js
@@ -1,21 +1,25 @@
-const cheerio = require('cheerio')
-const superagent = require('superagent');
+// const cheerio = require('cheerio')
+const superagent = require('superagent')
+const { JSDOM } = require('jsdom')
 
 class Crawler {
   constructor({ uri }) {
     this.url = uri
-    this.options = { uri, transform: body => cheerio.load(body) }
+    // this.options = { uri, transform: body => cheerio.load(body) }
   }
-  async crawl () {
+  /* async crawl () {
     return superagent.get(this.options)
       .then($ => $)
       .catch(err => console.log(err))
-  }
+  } */
+  // return html text
   async request () {
     return superagent.get(this.url)
   }
-  async load (html) {
-    return cheerio.load(html)
+  // return dom
+  async load (html, dangerously = false) {
+    // return cheerio.load(html)
+    return new JSDOM(html, { runScripts: dangerously ? 'dangerously' : '' })
   }
 }
 
