2a0bc54ec262536519c4b79fc836fbe075c18eb8
1099216788@qq.com <1099216788@qq.com>
2020-09-25 19:21:07 +0800

    修改页面

--- client/pages/toolkit/wechatqrcode.vue
+++ client/pages/toolkit/wechatqrcode.vue
@@ -4,36 +4,42 @@
     <div>
       <div class="param-wrap">
         <div>
-          <el-input placeholder="appId" v-model="form.appId" clearable>
+          <el-input placeholder="appId"
+                    v-model="form.appId"
+                    clearable>
           </el-input>
-          <el-input placeholder="appSecrct" v-model="form.appSecret" clearable>
+          <el-input placeholder="appSecrct"
+                    v-model="form.appSecret"
+                    clearable>
           </el-input>
-          <el-input placeholder="小程序页面page" v-model="form.page" clearable>
+          <el-input placeholder="小程序页面page"
+                    v-model="form.page"
+                    clearable>
           </el-input>
-          <el-input
-            placeholder="本地目录的绝对路径"
-            v-model="form.path"
-            clearable
-          >
+          <el-input placeholder="参数前缀（例如: 1664-）"
+                    v-model="form.prefix"
+                    clearable>
+          </el-input>
+          <el-input placeholder="本地目录的绝对路径"
+                    v-model="form.path"
+                    clearable>
           </el-input>
         </div>
         <div>
-          <el-input
-            placeholder="scene参数, 批量生成用回车分隔"
-            type="textarea"
-            v-model="form.scenes"
-          ></el-input>
+          <el-input placeholder="scene参数, 批量生成用回车分隔"
+                    type="textarea"
+                    v-model="form.scenes"></el-input>
         </div>
       </div>
-      <el-button class="submit-btn" @click="submit" type="primary"
-        >确定</el-button
-      >
+      <el-button class="submit-btn"
+                 @click="submit"
+                 type="primary">确定</el-button>
     </div>
   </section>
 </template>
 
 <script>
-import { Loading } from 'element-ui'
+import { Loading } from 'element-ui';
 
 export default {
   data() {
@@ -44,47 +50,48 @@ export default {
         page: '',
         path: '',
         scenes: '',
-      },
-    }
+        prefix: ''
+      }
+    };
   },
   mounted() {
-    const formData = this.$util.getStorage('formData')
+    const formData = this.$util.getStorage('formData');
     if (formData) {
-      this.form = formData
+      this.form = formData;
     }
   },
   methods: {
     async submit() {
-      let formData = Object.assign({}, this.form)
-      this.$util.setStorage('formData', formData)
+      let formData = Object.assign({}, this.form);
+      this.$util.setStorage('formData', formData);
 
       if (!formData.appId) {
         this.$message({
           type: 'warning',
-          message: '需要输入小程序appId',
-        })
-        return
+          message: '需要输入小程序appId'
+        });
+        return;
       }
       if (!formData.appSecret) {
         this.$message({
           type: 'warning',
-          message: '需要输入小程序appSecret',
-        })
-        return
+          message: '需要输入小程序appSecret'
+        });
+        return;
       }
       if (!formData.appSecret) {
         this.$message({
           type: 'warning',
-          message: '需要输入小程序appSecret',
-        })
-        return
+          message: '需要输入小程序appSecret'
+        });
+        return;
       }
       if (!formData.path) {
         this.$message({
           type: 'warning',
-          message: '需要输入本地目录的绝对路径',
-        })
-        return
+          message: '需要输入本地目录的绝对路径'
+        });
+        return;
       }
 
       if (Object.keys(formData)) {
@@ -92,26 +99,27 @@ export default {
           .replace(/[\f\t\v]/g, '') //匹配空白字符
           .split(/[\r\n]/)
           .filter(item => item != '')
-          .map(item => `channel=jsb-${item}`)
-        const loadingIns = Loading.service({ fullscreen: true })
-        const result = await this.$service.getWxaCodeLimit(formData)
-        loadingIns.close()
-        console.log(result)
+          .map(item => `channel=${formData.prefix}${item}`);
+
+        const loadingIns = Loading.service({ fullscreen: true });
+        const result = await this.$service.getWxaCodeLimit(formData);
+        loadingIns.close();
+        console.log(result);
         if (result.code === 1) {
           this.$message({
             message: `请在${formData.path}下查看`,
-            type: 'success',
-          })
-          return
+            type: 'success'
+          });
+          return;
         }
         if (result.code === 0) {
-          this.$message.error(result.err.errMsg)
+          this.$message.error(result.err.errMsg);
         }
       }
-    },
+    }
   },
-  watch: {},
-}
+  watch: {}
+};
 </script>
 
 <style>
@@ -154,6 +162,6 @@ export default {
 }
 .param-wrap textarea {
   width: 100%;
-  height: 190px !important;
+  height: 240px !important;
 }
 </style>
